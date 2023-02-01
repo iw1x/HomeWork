8f83edd82e2eebcd6679ec4b3fe7ed81f0c2d1f7
Dima <shlemov198@gmail.com>
2023-02-01 19:48:30 +0500

    homework

---
+++ Задача 15/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+
+
+Console.Clear();
+
+Console.Write("Введите номер дня недели: ");
+int num = int.Parse(Console.ReadLine()!);
+if (num>7)
+{
+    Console.WriteLine("В неделе всего 7 дней!");
+}
+
+else if (num < 6)
+{
+    Console.WriteLine($"{num} -> нет");
+}
+else Console.WriteLine($"{num} -> да");
---
+++ Задача 8/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+Console.Clear();
+
+Console.WriteLine($"Введите число ");
+int a = int.Parse(Console.ReadLine()!);
+int count = 2;
+while (count <= a)
+{
+Console.Write($"{count} ");
+
+count = count + 2; 
+}
