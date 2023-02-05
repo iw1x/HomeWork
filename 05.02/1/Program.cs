267b8033e0ba51edbb814ce6c8dac6aa6ce40696
Dima <shlemov198@gmail.com>
2023-02-05 19:52:55 +0500

    05.02

---
+++ 05.02/1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Подсчитать сумму цифр в числе 
+// 6547 => 6 + 5 + 4 + 7 = 22
+// 6547 => 7 + 4 + 5 + 6 = 22
+//6547 % 10 = нужно делить на 10 до тех пор, пока не останется одно число
+
+
+Console.Write("Введите число ");
+int num = int.Parse(Console.ReadLine()!);
+int num1 = num % 10;
+int sum = 0;
+
+while (num / 10 != 0)
+{
+    num = num / 10;
+    sum = sum + num % 10;
+}
+Console.WriteLine(sum+num1);
---
+++ 05.02/2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N  (1*2*3*4=24) 
+//4 => 1 * 2 * 3 * 4 = 24
+
+
+Console.Write("Введите число ");
+int num = int.Parse(Console.ReadLine()!);
+int count = 1;
+int a = 1;
+
+while (a <= num)
+{
+    count = count * a;
+    Console.Write($"{a} ");
+    a++;
+    
+}
+Console.WriteLine($"= {count}");
+    
+
+
+
+
+
+
+
