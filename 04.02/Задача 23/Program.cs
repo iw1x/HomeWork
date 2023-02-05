b49ea28042623886bf23e94bf8620dde98d1b020
Dima <shlemov198@gmail.com>
2023-02-05 19:56:20 +0500

    04.02

---
+++ 04.02/Задача 23/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+int count = 1;
+while (count <= a)
+{
+    Console.Write(count * count * count);
+    if (count != a)
+        {
+            Console.Write(", ");
+        }
+
+    count++;
+}
